b784e00e1b1258c8bd44e0ca33eb251549f57a0f
Lukasz Janusz <lukasz.t.janusz@gmail.com>
2021-06-15 15:23:32 +0200

    add class 'active' to the clicked link

--- js/script.js
+++ js/script.js
@@ -4,14 +4,29 @@
     console.log('links:', links);
   });*/
   const titleClickHandler = function(event){
+    event.preventDefault();
+    const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
+    
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
 
   /* add class 'active' to the clicked link */
+  console.log('clickedElement: ', clickedElement);
+  console.log('clickedElement (with plus): ' + clickedElement);
+  clickedElement.classList.add('active');
 
-  /* remove class 'active' from all articles */
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active');
+for(let activeArticle of activeArticles) {
+  activeArticle.classList.remove('active')
+}
 
   /* get 'href' attribute from the clicked link */
 
@@ -25,3 +40,4 @@
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
+  
